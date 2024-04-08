29239360bc5de114197bd11dca8e684e53d2a73a
HoaNguyenThu12 <hoanguyenthu.12hnt@gmail.com>
2024-04-08 10:23:38 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // App.js
-import React, { useContext } from 'react';
+import React from 'react';
 import CustomNavbar from './compents/Navbar';
 import { ThemeProvider } from './compents/ThemeContext';
-import { ThemeContext } from './compents/ThemeContext';
 
 
 function App () {
-  const { theme} = useContext(ThemeContext);
   return (
-    <ThemeProvider bg={theme === 'dark' ? 'dark' : 'light'} variant={theme === 'dark' ? 'dark' : 'light'} expand="lg">
+    <ThemeProvider>
       <div className="App">
         <CustomNavbar></CustomNavbar>
       </div>
